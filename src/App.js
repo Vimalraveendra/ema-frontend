3451d05a1c907c656606decbcbac51fff2927d8b
Vimal Raveendran <vimalraveendra@gmail.com>
2024-05-17 19:31:40 +0200

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import EmployeeList from './components/EmployeeList/EmployeeList';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="text-center">
+      <Header/>
        <EmployeeList/>
     </div>
   );
