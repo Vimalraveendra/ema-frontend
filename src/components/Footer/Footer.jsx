dd986fb33218080f3e7f19cc9f2be4cf80e1078e
Vimal Raveendran <vimalraveendra@gmail.com>
2024-05-20 21:50:47 +0200

    added Footer component & its styles

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.styles.css"
+
+const Footer= () => {
+  return (
+    <div>
+       <footer className='footer'>
+          <span>All rights reserved 2024 by Code</span>
+       </footer>
+    </div>
+  )
+}
+
+export default Footer;
