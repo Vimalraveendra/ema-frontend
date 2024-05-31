39a9fbbd04edc6c8c852c96dba9446cfd11c5918
Vimal Raveendran <vimalraveendra@gmail.com>
2024-05-31 19:41:28 +0200

    added UpdateEmployee functionality

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const Employee = ({id,firstName,lastName,email,updateEmployee,removeEmployee}) => {
+const Employee = ({id,firstName,lastName,email,updateEmployee}) => {
+  const onUpdateEmployee=()=>{
+    updateEmployee(id)
+  }
   return (
     <tr>
     <td>{id}</td>
     <td>{firstName}</td>
     <td>{lastName}</td>
     <td>{email}</td>
+    <button className='btn btn-info ' onClick={onUpdateEmployee}>Update</button>
  </tr>
   )
 }
